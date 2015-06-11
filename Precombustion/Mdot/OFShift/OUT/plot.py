b2bca3642c1cdf3b2722c584376699b965b5096d
三島源生 <Gmishima@mishimagenseis-iMac.local>
2015-06-12 07:25:47 +0900

    compiler

--- Precombustion/Mdot/OFShift/OUT/plot.py
+++ Precombustion/Mdot/OFShift/OUT/plot.py
@@ -18,7 +18,22 @@ plt.title('MdotO:0.327[kg/s]')
 #plt.legend(('MdotO:%s[kg/s]'%float(MdotO),))
 plt.xlabel('Time[s]')
 plt.ylabel(Element)
-#plt.ylim([0,100])
+plt.ylim([0.5,0.8])
+plt.savefig(OutputDirectry+Prefix+Element+".png")
+plt.close()
+#}}}
+Element  = 'Pres'
+#set{{{
+data = sp.genfromtxt(InputDirectry+Prefix+Element+'.d',delimiter=',')
+xval = data[:,0]
+yval = data[:,1]
+plt.plot(xval,yval)
+plt.grid()
+plt.title('MdotO:0.327[kg/s]')
+#plt.legend(('MdotO:%s[kg/s]'%float(MdotO),))
+plt.xlabel('Time[s]')
+plt.ylabel(Element)
+plt.ylim([0.5,1.1])
 plt.savefig(OutputDirectry+Prefix+Element+".png")
 plt.close()
 #}}}
@@ -94,7 +109,7 @@ plt.title('MdotO:0.327[kg/s]')
 #plt.legend(('MdotO:%s[kg/s]'%float(MdotO),))
 plt.xlabel('Time[s]')
 plt.ylabel(Element)
-#plt.ylim([0,100])
+plt.ylim([0.5,1.1])
 plt.savefig(OutputDirectry+Prefix+Element+".png")
 plt.close()
 #}}}
