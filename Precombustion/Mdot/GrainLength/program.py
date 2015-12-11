d3ac063ff2b79fc94a0e938f1c8b2e26199ac323
三島源生 <Gmishima@mishimagenseis-iMac.local>
2015-12-11 23:25:01 +0900

    length

--- Precombustion/Mdot/GrainLength/program.py
+++ Precombustion/Mdot/GrainLength/program.py
@@ -9,18 +9,20 @@ import math
 #Mass:kg
 #Time:s
 
-Time = 10.0            #[s]
+Time = 100.0            #[s]
 dt = 0.1                #[s]
 now = 0.0               #[s]
-PhiInit = 30.0*10**(-3.0) #[m]
+PhiInit = 100.0*10**(-3.0) #[m]
 Dia = PhiInit
-MdotO = 0.0231           #[kg/s]
-Length = 1.0*10**(-3.0)  #[m] Initial
+MdotO = 10.0           #[kg/s]
+Length = 10.0*10**(-3.0)  #[m] Initial
+border = (Dia/4)/Length
 RhoF = 1.18*10**3.0
 PreA = Dia**2.0*math.pi/4.0
 AllLength = np.array([])
 AllOF = np.array([])
-for i in range(0, 100):
+AllDia = np.array([])
+for i in range(0, 500):
 	dL = float(i)*10**(-5.0)
 	Length = Length + dL
 	now = 0.0
@@ -30,9 +32,13 @@ for i in range(0, 100):
 	PreA = Dia**2.0*math.pi/4.0
 	while now <= Time:
 		now = now+dt
+		border = (Dia/4)/Length
 		A = Dia**2.0*math.pi/4.0 #[m2]
 		Go = MdotO/A             #[kg/sm2]
-		rdot = 0.1358*Go**(0.4161)*10**-3  #[m/s] 15
+		rdot_Mid = 0.03699*Go**(0.5797)*10**-3
+		rdot_Bot = 0.2649*Go**(0.3701)*10**-3
+		rdot = (1-border)*rdot_Mid+border*rdot_Bot
+		#rdot = 0.1358*Go**(0.4161)*10**-3  #[m/s] 15
 		#rdot = 0.00765*Go**(0.9487)*10**-3  #[m/s] 7.5
 		dr = rdot*dt             #[m]
 		dA = A-PreA              #[m2]
@@ -48,10 +54,19 @@ for i in range(0, 100):
 	OF =  MdotO/AvMdotF
 	#print Length,OF
 	AllOF = np.append(AllOF,OF)
+	AllDia = np.append(AllDia,Dia)
 #print AllLength,AllOF
-plt.plot(AllLength,AllOF)
+print border,rdot_Mid,rdot_Bot,rdot,Dia
+fig, ax1 = plt.subplots()
+ax1.plot(AllLength,AllOF)
+plt.ylim([0,100])
+plt.ylabel('O/F')
+ax2 = ax1.twinx()
+ax2.plot(AllLength,AllDia,color='r')
+plt.ylim([0,1])
+plt.ylabel('Dia[m]')
+#plt.plot(AllLength,AllOF)
+#plt.plot(AllLength,AllDia)
 plt.grid()
 plt.xlabel('Length[m]')
-plt.ylabel('O/F')
-plt.ylim([0,100])
 plt.show()
